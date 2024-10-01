9c8b23ee85b7145414596ed8ec85bb4d38c48869
Miguelote <al066382@uacam.mx>
2024-09-30 20:26:27 -0600

    first commit

---
+++ App.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# MySqlConnection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask_contacts'
+mysql = MySQL(app)
+
+# settings
+app.secret_key = 'mysecretkey'
+
+@app.route('/')
+def Index():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM contacts')
+    data = cursor.fetchall()
+    return render_template('index.html', contacts = data)
+
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        #Escribimos la consulta
+        cursor.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)',
+        (fullname, phone, email))
+        #Ejecutamos la consulta
+        mysql.connection.commit()
+    
+        flash('Contact Added Successfully')
+        return redirect(url_for('Index'))
+
+@app.route('/edit/<id>')
+def get_contact(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM contacts WHERE id_contacts = %s', (id,))
+    data = cursor.fetchall()
+    print(data[0])
+    return render_template('edit_contacts.html', contact = data[0])
+
+@app.route('/update/<id>', methods = ['post'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute("""
+        UPDATE contacts
+        SET fullname = %s, 
+            email = %s,
+            phone = %s
+        WHERE id_contacts = %s
+        """, (fullname, email, phone, id))
+        mysql.connection.commit()
+        flash('Contact Updated Successfully')
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cursor = mysql.connection.cursor()
+    # Usamos %s para evitar SQL injection
+    cursor.execute('DELETE FROM contacts WHERE id_contacts = %s', (id,))
+    mysql.connection.commit()
+    flash('Contact Removed Successfully')
+    return redirect(url_for('Index'))
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
+    
+
+
